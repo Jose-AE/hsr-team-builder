3ecdbbec8803bda46c46a50a6f6f716712b45c2e
Antosik <grig_anton96@mail.ru>
2024-12-30 00:14:04 +0300

    feat(scraper): refactor scraper a bit

--- scraper/utils/getTeams.ts
+++ scraper/utils/getTeams.ts
@@ -1,22 +1,20 @@
 import axios from "axios";
-import jsdom from "jsdom";
-import util from "util";
+import { JSDOM } from "jsdom";
 
-import { CharacterData, getCharacters } from "./getCharacters";
-const { JSDOM } = jsdom;
+import type { CharacterData } from "./getCharacters";
 
 interface TeamData {
   primaryCharacter: CharacterData;
   characters: CharacterData[];
 }
 
-function getTeamDivs(dom: jsdom.JSDOM): Element[] {
+function getTeamDivs(dom: JSDOM): Element[] {
   // Select all elements with class "e-con-inner"
   const elementsWithClass =
     dom.window.document.getElementsByClassName("e-con-inner");
 
   // Filter elements based on the number of child elements and that arent widgets
-  const teamDivs = Array.from(elementsWithClass).filter((element) => {
+  return Array.from(elementsWithClass).filter((element) => {
     if (element.children.length === 4) {
       return Array.from(element.children).every(
         (child) => !child.className.includes("widget")
@@ -24,10 +22,6 @@ function getTeamDivs(dom: jsdom.JSDOM): Element[] {
     }
     return false;
   });
-
-  //console.log(`Number of teams: ${teamDivs.length}`);
-
-  return teamDivs;
 }
 
 function getCharactersInTeamDiv(
@@ -46,36 +40,36 @@ function getCharactersInTeamDiv(
       tagText?.includes("Support") ||
       tagText?.includes("Tank") ||
       tagText?.includes("Shield")
-    )
+    ) {
       continue;
-    //if (i % 2 == 1) continue;
+    }
 
-    const charToAdd = chars.find((chr) => {
-      //console.log(`${chr.name} === ${charNameTags[i].textContent}`);
-      return charNameTags[i].textContent?.includes(chr.name);
-    });
-
-    if (charToAdd) charactersData.push(charToAdd);
-    else {
-      //console.log("------" + charNameTags[i].textContent);
+    const charToAdd = chars.find((chr) =>
+      charNameTags[i].textContent?.includes(chr.name)
+    );
+    if (charToAdd) {
+      charactersData.push(charToAdd);
     }
   }
-
-  //console.log(emTagsFollowedByBr[0].textContent);
 
   return charactersData;
 }
 
 function generateTeamsDataFromDom(
-  dom: jsdom.JSDOM,
+  dom: JSDOM,
   chars: CharacterData[],
   mainChar: CharacterData
 ): TeamData[] {
   const teamDivs = getTeamDivs(dom);
+  if (!teamDivs.length) {
+    return [];
+  }
+
   const teams: TeamData[] = [];
 
   for (let teamDiv of teamDivs) {
     const teamChars = getCharactersInTeamDiv(teamDiv, chars);
+
     if (teamChars.length === 4) {
       teams.push({ characters: teamChars, primaryCharacter: mainChar });
       console.log("\x1b[32m" + `Added ${mainChar.name} team`);
@@ -90,35 +84,44 @@ function generateTeamsDataFromDom(
   return teams;
 }
 
-export async function getTeams(): Promise<TeamData[]> {
+async function loadCharacterTeams(
+  char: CharacterData,
+  allCharacters: CharacterData[]
+) {
+  return axios
+    .get(
+      `https://genshinlab.com/honkai-star-rail-team/${char.name_id}-team-guide/`
+    )
+    .then((response) => {
+      const dom = new JSDOM(response.data);
+      return generateTeamsDataFromDom(dom, allCharacters, char);
+    });
+}
+
+export async function getTeams(
+  chars: CharacterData[] = []
+): Promise<TeamData[]> {
   const teams: TeamData[] = [];
-  const chars = await getCharacters();
 
   for (let char of chars) {
-    await axios
-      .get(
-        `https://genshinlab.com/honkai-star-rail-team/${char.name_id}-team-guide/`
-      )
-      .then((response) => {
-        const dom = new JSDOM(response.data);
-        const charTeams = generateTeamsDataFromDom(dom, chars, char);
+    // Initial teams load
+    await loadCharacterTeams(char, chars)
+      .then((charTeams) => {
         teams.push(...charTeams);
-        //console.log(util.inspect(charTeams, true, null, true));
       })
-      .catch(async (error) => {
-        //console.log(`error getting teams for ${char.name_id}`);
-        await axios
-          .get(
-            `https://genshinlab.com/honkai-star-rail-team/${char.name_id.replace(
-              "-",
-              ""
-            )}-team-guide/`
-          )
-          .then((response) => {
-            const dom = new JSDOM(response.data);
-            teams.push(...generateTeamsDataFromDom(dom, chars, char));
+      .catch(async () => {
+        // Fallback
+        await loadCharacterTeams(
+          {
+            ...char,
+            name_id: char.name_id.replace("-", ""),
+          },
+          chars
+        )
+          .then((charTeams) => {
+            teams.push(...charTeams);
           })
-          .catch((error) => {
+          .catch(() => {
             console.log(`Error getting teams for ${char.name_id}`);
           });
       });
